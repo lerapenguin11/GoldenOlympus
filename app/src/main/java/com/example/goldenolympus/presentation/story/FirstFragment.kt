8e96d49f932d3e1080df1ab914726d30993cca37
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-10-06 15:02:13 +0500

    screen layout first

--- app/src/main/java/com/example/goldenolympus/presentation/story/FirstFragment.kt
+++ app/src/main/java/com/example/goldenolympus/presentation/story/FirstFragment.kt
@@ -6,14 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.goldenolympus.R
+import com.example.goldenolympus.databinding.FragmentFirstBinding
+import com.example.goldenolympus.utilits.replaceFragmentMain
 
 class FirstFragment : Fragment() {
+    private var _binding : FragmentFirstBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.btTap.setOnClickListener { replaceFragmentMain(SecondFragment()) }
+
+        return binding.root
     }
 }
