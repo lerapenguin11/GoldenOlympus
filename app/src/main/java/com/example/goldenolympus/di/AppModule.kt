a43a4f0ecda29e2a99412d2c9d092378ca1bfd64
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-10-07 19:45:35 +0500

    SettingsViewModel.kt fix

--- app/src/main/java/com/example/goldenolympus/di/AppModule.kt
+++ app/src/main/java/com/example/goldenolympus/di/AppModule.kt
@@ -2,13 +2,16 @@ package com.example.goldenolympus.di
 
 import android.app.Application
 import com.example.goldenolympus.viewmodel.SettingsViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel<SettingsViewModel> {
+        val appContext = androidContext().applicationContext
+
         SettingsViewModel(
-            application = Application()
+            application = get()
         )
     }
 }
