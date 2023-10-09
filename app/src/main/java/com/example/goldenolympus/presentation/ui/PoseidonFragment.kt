2e06a7fdfc1739a43be08bfcab3c7f437a9a0136
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-10-10 00:36:34 +0500

    add timer

--- app/src/main/java/com/example/goldenolympus/presentation/ui/PoseidonFragment.kt
+++ app/src/main/java/com/example/goldenolympus/presentation/ui/PoseidonFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.graphics.*
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +25,12 @@ import java.util.*
 class PoseidonFragment : Fragment() {
     private var _binding : FragmentPoseidonBinding? = null
     private val binding get() = _binding!!
+    private var seconds = 0
+    private var isRunning = false
+    private var wasRunning = false
+
+    private lateinit var handler : Handler
+    private lateinit var runnable: Runnable
 
     enum class Color {
         WHITE, GRAY, BLACK
@@ -45,22 +52,63 @@ class PoseidonFragment : Fragment() {
         requireActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics)
         _binding = FragmentPoseidonBinding.inflate(inflater, container, false)
 
+        handler = Handler()
+
+        runnable = Runnable {
+            seconds++
+            binding.tvSec.text = formatTime(seconds)
+            handler.postDelayed(runnable, 1000)
+        }
+
+        startTimer()
+
+        binding.icPause.setOnClickListener {
+            pauseTimer()
+        }
+
         val params: ViewGroup.LayoutParams = binding.mazeWrapper.getLayoutParams()
         params.height = Math.floor(displaymetrics.heightPixels * 0.7).toInt()
         binding.mazeWrapper.setLayoutParams(params)
 
         binding.newMazeButton.setOnClickListener(View.OnClickListener { createMaze() })
         binding.newMazeButton.performClick()
-
-
         binding.icArrow.setOnClickListener { replaceFragmentMain(MenuFragment()) }
-
 
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (wasRunning) {
+            startTimer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        wasRunning = isRunning
+        pauseTimer()
+    }
+
+    private fun startTimer() {
+        isRunning = true
+        handler.postDelayed(runnable, 1000)
+        binding.icPause.isEnabled = true
+    }
+
+    private fun pauseTimer() {
+        isRunning = false
+        handler.removeCallbacks(runnable)
+        binding.icPause.isEnabled = false
+    }
+
+    private fun formatTime(seconds: Int): String {
+        val minutes = (seconds % 3600) / 60
+        val secs = seconds % 60
+        return String.format("%02d:%02d", minutes, secs)
+    }
+
     fun createMaze() {
-        // First remove any existing MazeView and FingerLine
         if (mMazeView != null) {
             (mMazeView!!.parent as ViewGroup).removeView(mMazeView)
         }
@@ -69,12 +117,6 @@ class PoseidonFragment : Fragment() {
         }
         mazeSize = 10
         mMazeView = MazeView(requireContext(), mazeSize)
-
-        // Trace the path from start to farthestVertex using the line of predecessors,
-        // apply this information to form an array of rectangles
-        // which will be passed on to fingerLine view
-        // where the line has to pass.
-        // The array be checked against the drawn line in FingerLine.
         val solutionAreas = Array(mMazeView!!.lengthOfSolutionPath) {
             IntArray(
                 4
@@ -82,7 +124,6 @@ class PoseidonFragment : Fragment() {
         }
         var currentVertexKey: Int
         val totalMazeWidth = displaymetrics.widthPixels - PADDING
-        // int totalMazeHeight = totalMazeWidth;
         val cellSide = totalMazeWidth / mazeSize
         var row: Int
         var column: Int
@@ -92,8 +133,6 @@ class PoseidonFragment : Fragment() {
         var bottomRightY: Int
         for (i in 0 until mMazeView!!.lengthOfSolutionPath) {
             currentVertexKey = mMazeView!!.listOfSolutionVertecesKeys[i]
-
-            // Translate vertex key to location on screen
             row = currentVertexKey / mazeSize
             column = currentVertexKey % mazeSize
             topLeftX = PADDING / 2 + column * cellSide - FAT_FINGERS_MARGIN
@@ -105,8 +144,6 @@ class PoseidonFragment : Fragment() {
         binding.mazeWrapper.addView(mMazeView)
         mFingerLine = FingerLine(requireContext(), null, solutionAreas)
         binding.mazeWrapper.addView(mFingerLine)
-
-        // Add start arrow pic
         val startCellArrowX =
             solutionAreas[mMazeView!!.lengthOfSolutionPath - 1][0] + 12 + FAT_FINGERS_MARGIN
         val startCellArrowY =
@@ -116,7 +153,6 @@ class PoseidonFragment : Fragment() {
         binding.arrow.y = startCellArrowY.toFloat()
         binding.arrow.visibility = View.VISIBLE
 
-        // Add strawberry pic
         val endCellStrawberryX = solutionAreas[0][0] + 8 + FAT_FINGERS_MARGIN
         val endCellStrawberryY = solutionAreas[0][1] + 10 + FAT_FINGERS_MARGIN
         binding.strawberry.setX(endCellStrawberryX.toFloat())
@@ -130,15 +166,5 @@ class PoseidonFragment : Fragment() {
         val newArray = arrayOfNulls<Int>(arrayList.size)
         arrayList.toArray(newArray)
         return newArray
-    }
-
-    fun startGameSolvedAnimation() {
-        val animation: Animation = AlphaAnimation(1f, 0f)
-        animation.duration = 700
-        animation.interpolator = LinearInterpolator()
-        animation.repeatCount = 3
-        animation.repeatMode = Animation.REVERSE
-
-        //binding.strawberry.startAnimation(animation)
     }
 }
